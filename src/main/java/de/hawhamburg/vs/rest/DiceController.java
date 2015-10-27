271b686f4692aa88be11fe346ebc15c1eb6d2470
Tim Hagemann <tim@infinityblade.de>
2015-10-27 14:39:45 +0100

    Added logging and base for last task.

--- src/main/java/de/hawhamburg/vs/rest/DiceController.java
+++ src/main/java/de/hawhamburg/vs/rest/DiceController.java
@@ -2,6 +2,8 @@ package de.hawhamburg.vs.rest;
 
 import de.hawhamburg.vs.rmi.shared.DiceRMI;
 import de.hawhamburg.vs.rmi.shared.Roll;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import spark.Request;
 import spark.Response;
 
@@ -12,17 +14,30 @@ import static spark.Spark.*;
 
 public final class DiceController {
     private final DiceRMI dice;
+    private static final Logger log = LoggerFactory.getLogger(DiceController.class);
 
     public DiceController(DiceRMI dice) {
         this.dice = dice;
         get("/dice", this::handleDiceGet, json());
+        get("/deck/:gameId/community", this::handleCommunityDeck, json());
+        get("/deck/:gameId/chance", this::handleChanceDeck, json());
     }
 
     public Roll handleDiceGet(Request request, Response response) {
         try {
-            return dice.roll();
+            Roll roll = dice.roll();
+            log.info(roll.toString());
+            return roll;
         } catch (RemoteException e) {
             return null;
         }
+    }
+
+    public Object handleCommunityDeck(Request request, Response response) {
+        return null;
+    }
+
+    public Object handleChanceDeck(Request request, Response response) {
+        return null;
     }
 }
